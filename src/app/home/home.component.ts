9e2f4c2f64b0c08740c93f06a208de84a6c2e0ae
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-19 21:03:37 -0400

    more options for Observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,36 @@
+// OBSERVABLE CREATED WITH INTERVAL
+
+// import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { interval, Subscription } from 'rxjs'
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent implements OnInit, OnDestroy {
+
+//   private firstObsSubscr: Subscription
+
+//   constructor() { }
+
+//   ngOnInit() {
+//     // a bit like setInterval() 
+//     this.firstObsSubscr = interval(1000).subscribe(count => {
+//       console.log(count)
+//     })
+//   }
+
+//   ngOnDestroy() {
+//     this.firstObsSubscr.unsubscribe()
+//   }
+
+// }
+
+// CUSTOM OBSERVABLE - SANE AS ABOVE, BUT THIS TIME FROM SCRATCH
+
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs'
+import { Subscription, Observable } from 'rxjs'
 
 @Component({
   selector: 'app-home',
@@ -13,10 +44,28 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    // a bit like setInterval() 
-    this.firstObsSubscr = interval(1000).subscribe(count => {
-      console.log(count)
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0
+      setInterval(() => {
+        observer.next(count)
+        if (count === 3) {
+          observer.complete()
+        }
+        if (count > 4) {
+          // it dies at 4, no need to unsubscribe
+          observer.error(new Error('count is bigger than 4!'))
+        }
+        count++
+        // observer.error()
+        // observer.complete()
+      }, 1000)
     })
+
+    this.firstObsSubscr = customIntervalObservable.subscribe(
+      data => console.log(data), 
+      error => alert(error.message),
+      () => console.log('completed')
+    )
   }
 
   ngOnDestroy() {
