4146f2ec083128dc96b604a25e6b9a5a9b7020f1
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-19 20:42:57 -0400

    interval observable created and destroyed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscr: Subscription
 
   constructor() { }
 
   ngOnInit() {
+    // a bit like setInterval() 
+    this.firstObsSubscr = interval(1000).subscribe(count => {
+      console.log(count)
+    })
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscr.unsubscribe()
   }
 
 }
